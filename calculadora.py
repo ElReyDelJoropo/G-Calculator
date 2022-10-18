0882f91aea6dc8813991e18fd518bba8893321a7
Gustavo Serrano <gustavo.serrano.dev@proton.me>
2022-10-18 01:18:50 -0400

    more visual improvements

--- calculadora.py
+++ calculadora.py
@@ -5,7 +5,7 @@ from tkinter import ttk
 class Calculator:
     def __init__(self) -> None:
         self.root = Tk(className="Calculator")
-        self.root.resizable(False,False)
+        self.root.resizable(False, False)
         self.mainframe = ttk.Frame(self.root)
 
         # Buffer contains current user input
@@ -53,28 +53,38 @@ class Calculator:
         # Numeric buttons 0-9
         for i in range(10):
             self.buttons[i] = Button(
-                self.mainframe, text=str(i), command=lambda x=str(i): self.putNumber(x), **self.style_button_common,**self.style_button_dracula1
+                self.mainframe,
+                text=str(i),
+                command=lambda x=str(i): self.putNumber(x),
+                **self.style_button_common,
+                **self.style_button_dracula1
             )
         # Operators
         for operator in self.operators:
             self.buttons[operator] = Button(
                 self.mainframe,
                 text=operator,
-                command=lambda op=operator: self.putOperator(op),**self.style_button_common,**self.style_button_dracula2
+                command=lambda op=operator: self.putOperator(op),
+                **self.style_button_common,
+                **self.style_button_dracula2
             )
         # Function keys:
         for function in self.function_keys:
             self.buttons[function] = Button(
                 self.mainframe,
                 text=function,
-                command=lambda op=function: self.putFunction(op),**self.style_button_common,**self.style_button_dracula4
+                command=lambda op=function: self.putFunction(op),
+                **self.style_button_common,
+                **self.style_button_dracula4
             )
         # Special keys
         for key in self.special_keys:
             self.buttons[key] = Button(
                 self.mainframe,
                 text=key,
-                command=lambda func=self.special_keys[key]: func(),**self.style_button_common,**self.style_button_dracula3
+                command=lambda func=self.special_keys[key]: func(),
+                **self.style_button_common,
+                **self.style_button_dracula3
             )
 
     def layoutWidgets(self):
@@ -121,11 +131,33 @@ class Calculator:
 
     def setStyle(self):
         # Style based on Dracula colorscheme
-        self.style_button_common = {"background" : "#282A36", "font" : "Arial 14", "height" : 2, "width" : 5, "borderwidth" : 0, "relief" : FLAT}
-        self.style_button_dracula1 = {"foreground" : "#9AEDFE", "activebackground" : "#9AEDFE", "activeforeground": "white", "highlightbackground" : "white"}
-        self.style_button_dracula2 = {"foreground" : "#50FA7B", "activebackground" : "#50FA7B", "activeforeground": "white", "highlightbackground" : "white"}
-        self.style_button_dracula3 = {"foreground" : "#CAA9FA", "activebackground" : "#CAA9FA", "activeforeground": "white", "highlightbackground" : "white"}
-        self.style_button_dracula4 = {"foreground" : "#F1FA8C", "activebackground" : "#F1FA8C", "activeforeground": "white", "highlightbackground" : "white"}
+        self.style_button_common = {
+            "background": "#282A36",
+            "font": "Arial 14",
+            "height": 2,
+            "width": 5,
+            "borderwidth": 0,
+            "relief": FLAT,
+            "activeforeground": "white",
+            "highlightbackground": "#282A36",
+            "highlightcolor": "blue",
+        }
+        self.style_button_dracula1 = {
+            "foreground": "#9AEDFE",
+            "activebackground": "#9AEDFE",
+        }
+        self.style_button_dracula2 = {
+            "foreground": "#50FA7B",
+            "activebackground": "#50FA7B",
+        }
+        self.style_button_dracula3 = {
+            "foreground": "#CAA9FA",
+            "activebackground": "#CAA9FA",
+        }
+        self.style_button_dracula4 = {
+            "foreground": "#F1FA8C",
+            "activebackground": "#F1FA8C",
+        }
         self.style = ttk.Style()
         self.style.configure(
             "Dracula1.TLabel",
@@ -142,14 +174,11 @@ class Calculator:
             anchor=E,
         )
         self.style.configure(
-            "Dracula1.TFrame",
-            background="#4D4D4D",
-            foreground="#4D4D4D"
+            "Dracula1.TFrame", background="#4D4D4D", foreground="#4D4D4D"
         )
         self.mainframe.configure(style="Dracula1.TFrame")
         self.result.configure(style="Dracula1.TLabel")
         self.sub_result.configure(style="Dracula2.TLabel")
-
 
     def setKeybindings(self):
         for i in range(10):
